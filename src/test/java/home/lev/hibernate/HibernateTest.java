54cccddcbc370b7108e51eb043d219a86dc71d57
Lev999 <lev.magazinnik@gmail.com>
2015-07-16 14:21:21 -0300

    rolledBack OK

--- src/test/java/home/lev/hibernate/HibernateTest.java
+++ src/test/java/home/lev/hibernate/HibernateTest.java
@@ -5,11 +5,16 @@ import home.lev.hibernate.entityDAO.PersonDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/hibernate-context.xml")
+@Transactional
+@TransactionConfiguration(defaultRollback = true)
 public class HibernateTest {
 
     @Autowired
@@ -20,6 +25,9 @@ public class HibernateTest {
         Person p1 = new Person();
         p1.setName("Gongua");
         personDao.savePerson(p1);
+        for (Person person : personDao.getAllPersons()) {
+            System.out.println(person);
+        }
     }
 
 
